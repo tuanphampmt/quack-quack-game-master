1db8880b7e244c7ad370459b6257af3743042112
TUAN-PHAM\Admin <tuanpham31798@gmail.com>
2024-06-21 20:47:31 +0700

    hi

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const loadToken = require("./modules/loadToken");
-const ACCESS_TOKEN = loadToken();
+const ACCESS_TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjE1MTA1MywidGltZXN0YW1wIjoxNzE4OTY5MjMyMDI1LCJ0eXBlIjoxLCJpYXQiOjE3MTg5NjkyMzIsImV4cCI6MTcxOTU3NDAzMn0.8M3AcQYGxl1bHeePqrEnDRcoT1MCVB2eQSKWKfiPRPo";
 const collectGoldenDuck = require("./scripts/collectGoldenDuck");
 
 app.get("/home", (req, res) => {
