5c6d95dda8df31ed1d7e552b72bcbd49511762a8
TUAN-PHAM\Admin <tuanpham31798@gmail.com>
2024-06-21 23:43:45 +0700

    hi

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const cron = require("node-cron");
+const collectGoldenDuck = require("./scripts/collectGoldenDuck");
 const app = express();
 const ACCESS_TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjE1MTA1MywidGltZXN0YW1wIjoxNzE4OTY5MjMyMDI1LCJ0eXBlIjoxLCJpYXQiOjE3MTg5NjkyMzIsImV4cCI6MTcxOTU3NDAzMn0.8M3AcQYGxl1bHeePqrEnDRcoT1MCVB2eQSKWKfiPRPo";
-const collectGoldenDuck = require("./scripts/collectGoldenDuck");
 
-app.get("/", (req, res) => {
-  res.status(200).json("Welcome, your app is working well");
+cron.schedule("*/10 * * * * *", () => {
   collectGoldenDuck(ACCESS_TOKEN);
 });
+
+// app.get("/", (req, res) => {
+//   res.status(200).json("Welcome, your app is working well");
+//   collectGoldenDuck(ACCESS_TOKEN);
+// });
 app.listen(3000, () => {
   console.log("Server ready on port 3000.");
 });
