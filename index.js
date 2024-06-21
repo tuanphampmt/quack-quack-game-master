04fcc834a810a99ed12094ec0595a21e236d05b7
TUAN-PHAM\Admin <tuanpham31798@gmail.com>
2024-06-21 20:44:01 +0700

    hi

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const loadToken = require("./modules/loadToken");
 const ACCESS_TOKEN = loadToken();
 const collectGoldenDuck = require("./scripts/collectGoldenDuck");
 
-app.get("/api", (req, res) => {
+app.get("/home", (req, res) => {
+  res.status(200).json("Welcome, your app is working well");
   collectGoldenDuck(ACCESS_TOKEN);
-  res.send("Express on Vercel");
 });
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
