10417c9a9783fb8fd9d2dfc7c95fc1d5bb1d0955
TUAN-PHAM\Admin <tuanpham31798@gmail.com>
2024-06-21 23:36:00 +0700

    hi

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const ACCESS_TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjE1MTA1MywidGltZXN0YW1wIjoxNzE4OTY5MjMyMDI1LCJ0eXBlIjoxLCJpYXQiOjE3MTg5NjkyMzIsImV4cCI6MTcxOTU3NDAzMn0.8M3AcQYGxl1bHeePqrEnDRcoT1MCVB2eQSKWKfiPRPo";
 const collectGoldenDuck = require("./scripts/collectGoldenDuck");
 
+app.get("/", (req, res) => {
+  res.status(200).json("Welcome, your app is working well");
+  collectGoldenDuck(ACCESS_TOKEN);
+});
 app.listen(3000, () => {
   console.log("Server ready on port 3000.");
-  collectGoldenDuck(ACCESS_TOKEN);
 });
 
 module.exports = app;
