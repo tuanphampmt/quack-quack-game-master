40545671c4e5ce89e3e2ca38f022967a0e326a04
TUAN-PHAM\Admin <tuanpham31798@gmail.com>
2024-06-21 23:52:01 +0700

    hi

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const ACCESS_TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjE1MTA1MywidGltZXN0YW1wIjoxNzE4OTY5MjMyMDI1LCJ0eXBlIjoxLCJpYXQiOjE3MTg5NjkyMzIsImV4cCI6MTcxOTU3NDAzMn0.8M3AcQYGxl1bHeePqrEnDRcoT1MCVB2eQSKWKfiPRPo";
 
 app.get("/", (req, res) => {
-  res.status(200).json("Welcome, your app is working well");
   cron.schedule("*/10 * * * * *", () => {
     collectGoldenDuck(ACCESS_TOKEN);
   });
