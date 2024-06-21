79afca3fc3400490a34bfaab696541832bbaf531
TUAN-PHAM\Admin <tuanpham31798@gmail.com>
2024-06-21 23:56:52 +0700

    hi

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const ACCESS_TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjE1MTA1MywidGltZXN0YW1wIjoxNzE4OTY5MjMyMDI1LCJ0eXBlIjoxLCJpYXQiOjE3MTg5NjkyMzIsImV4cCI6MTcxOTU3NDAzMn0.8M3AcQYGxl1bHeePqrEnDRcoT1MCVB2eQSKWKfiPRPo";
 
 app.get("/", (req, res) => {
-  cron.schedule("*/10 * * * * *", () => {
-    collectGoldenDuck(ACCESS_TOKEN);
-  });
+  res.status(200).json("Welcome, your app is working well");
 });
+
+collectGoldenDuck(ACCESS_TOKEN);
 app.listen(3000, () => {
   console.log("Server ready on port 3000.");
 });
