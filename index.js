db42ba35ee5325e8e28ef3c373df277dc23ddbf7
TUAN-PHAM\Admin <tuanpham31798@gmail.com>
2024-06-22 11:00:25 +0700

    HI

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
 collectGoldenDuck(ACCESS_TOKEN);
 
 app.listen(3000, () => {
-  console.log("Server ready on port 3000.");
+  console.log("Server ready on port 3000..");
 });
 
 module.exports = app;
